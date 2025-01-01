98064bbe6054491ce1d953af6ebf27b1d0092e46
AmirHossein Jahankheir Amlashi <amir9877h@gmail.com>
2025-01-01 21:28:07 +0330

    fix issue

--- components/FoodCard/RouteToDetail.tsx
+++ components/FoodCard/RouteToDetail.tsx
@@ -1,12 +1,20 @@
 "use client";
 import { useRouter } from "next/navigation";
 import styles from "./food-card.module.scss";
+import { ReactNode } from "react";
 
-const RouteToDetail = ({ id, children }) => {
+interface RouteToDetailProps {
+  id: string | number;
+  children: ReactNode;
+}
+
+const RouteToDetail = ({ id, children }: RouteToDetailProps) => {
   const router = useRouter();
   return (
     <div
-      className={[styles.container, "cursor-pointer hover:bg-slate-200"].join(" ")}
+      className={[styles.container, "cursor-pointer hover:bg-slate-200"].join(
+        " "
+      )}
       onClick={() => router.push(`/detail/${id}`)}
     >
       {children}
