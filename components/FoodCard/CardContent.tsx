69cbaede75b78492edab3cd11fcd949b9a1aafd9
AmirHossein Jahankheir Amlashi <amir9877h@gmail.com>
2025-01-03 16:31:47 +0330

    change foodCard style

--- components/FoodCard/CardContent.tsx
+++ components/FoodCard/CardContent.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import styles from "./food-card.module.scss";
 // import styles from "./food-card.module.scss";
 import { FoodCardProps } from ".";
 
@@ -8,11 +9,9 @@ const CartContent = (props: FoodCardProps) => {
     <>
       <h3 className=" font-bold text-2xl mb-3">{title}</h3>
       <Image src={imageUrl} alt={title} width={300} height={300} />
-      <div className="flex justify-between w-full py-5">
+      <div className={styles.descriptionContainer}>
         <p>{description}</p>
-        <button className="bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 border-b-4 border-orange-700 hover:border-orange-500 rounded max-sm:text-sm">
-          More Detail...
-        </button>
+        <button className={styles.button}>More Detail...</button>
       </div>
     </>
   );
